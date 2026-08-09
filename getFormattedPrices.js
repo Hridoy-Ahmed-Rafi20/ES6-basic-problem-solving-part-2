7a35ba5d9f94b026e18328dbd7c9d3b71dfdb219
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-08-09 09:24:55 +0600

    get formatted prices

--- getFormattedPrices.js
+++ getFormattedPrices.js
@@ -3,3 +3,12 @@ Given an array of numbers, return a new array where each price is formatted
 as a string prefixed with "$". 
 Input: [100, 250, 75]  
 Output: ["$100", "$250", "$75"]  */
+
+const getFormattedPrices = (prices) => {
+  const getFormatted = prices.map((currentPrice) => {
+    return `$${currentPrice}`;
+  });
+  return getFormatted;
+};
+let prices = [100, 250, 75];
+console.log(getFormattedPrices(prices));
