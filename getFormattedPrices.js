166c86b6d2cb3b3457b785086d2f831f2eb398f1
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-08-09 09:14:37 +0600

    first commit

---
+++ getFormattedPrices.js
@@ -0,0 +1,5 @@
+/* getFormattedPrices(prices) — map()  
+Given an array of numbers, return a new array where each price is formatted 
+as a string prefixed with "$". 
+Input: [100, 250, 75]  
+Output: ["$100", "$250", "$75"]  */
